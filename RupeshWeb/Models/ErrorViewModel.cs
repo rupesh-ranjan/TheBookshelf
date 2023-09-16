34b58e41ef9a829782966f6bcf9bc7b02a4bdd3e
Rupesh Ranjan <2068799@cognizant.com>
2023-09-17 02:58:34 +0530

    Add project files.

---
+++ RupeshWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RupeshWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
